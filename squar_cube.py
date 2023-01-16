88673915e48bf9a3cadd6e9309d23715d261c2cd
nithin <aannanithin@gmail.com>
2023-01-16 14:38:22 +0530

    commited

---
+++ squar_cube.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[11]:
+
+
+a=[2,4,6,8,10]
+print("square of list")
+for i in a:
+    print(i**2)
+print ('cube of list')
+for i in a:
+    print(i**2*i)
+    
+    
+    
+    
+
+
+# In[ ]:
+
+
+
+
